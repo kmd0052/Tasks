11ae099cad48d7dcf5dcc443a3ad25e736b8cb67
kmd0052 <kmd0052@mix.wvu.edu>
2021-01-28 16:42:43 -0500

    Kylie Dickinson Task 01 upload c

--- Task_01/task01.r
+++ Task_01/task01.r
@@ -34,3 +34,21 @@ file.path("folder1", "folder2")
 dir.create(file.path("testdir2", "testdir3"), recursive = TRUE)
 setwd(old.dir)
 2
+1
+3
+1:20
+pi:10
+15:1
+?':'
+seq()
+seq(1, 20)
+seq(0, 10, by=0.5)
+my_seq <- seq(5, 10, length=30)
+length(my_seq)
+1:length(my_seq)
+seq(along.with = my_seq)
+seq_along(my_seq)
+rep(0, times = 40)
+rep(c(0, 1, 2), times = 10)
+rep(c(0, 1, 2), each = 10)
+1
