2c9a8120f784923679a751ccd6343cf16458c52c
kmd0052 <kmd0052@mix.wvu.edu>
2021-01-28 15:59:14 -0500

    Kylie Dickinson Task 01 upload c

--- Task_01/task01.r
+++ Task_01/task01.r
@@ -1,25 +1,36 @@
-library (swirl)
+library(swirl)
+
+swirl()
+Kylie Dickinson
+2
+2
+swirl()
 Kylie Dickinson
 1
+no2
+2
 1
-1
-5 + 7 
-x <- 5 + 7
-x 
-y <- x - 3
-y
-z <- c(1.1, 9, 3.14)
-?c
-z
-(z, 555, z)
-c(z, 555, z)
-z * 2 + 100
-my_sqrt <- sqrt(z - 1)
-1
-my_div <- z / my_sqrt
-skip()
-c(1, 2, 3, 4) + c(0, 10)
-c(1, 2, 3, 4) + c(0, 10, 100)
-my_div
-
-
+2
+getwd()
+ls()
+x <- 9
+ls()
+list.files()
+?list.files
+args(list.files)
+old.dir <- getwd()
+dir.create("testdir")
+setwd("testdir")
+file.create("mytest.R")
+list.files()
+file.exists("mytest.R")
+file.info("mytest.R")
+file.rename("mytest.R", "mytest2.R")
+file.copy("mytest2.R", "mytest3.R")
+file.path("mytest3.R")
+file.("folder1", "folder2")
+file.path("folder1", "folder2")
+?dir.create
+dir.create(file.path("testdir2", "testdir3"), recursive = TRUE)
+setwd(old.dir)
+2
