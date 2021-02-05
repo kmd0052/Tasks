51254609aaafed4194abc2c857bdf02598240e48
kmd0052 <kmd0052@mix.wvu.edu>
2021-02-04 20:16:46 -0500

    Kylie Dickinson Task 02 upload b

--- Task_01/task01.r
+++ Task_01/task01.r
@@ -28,4 +28,29 @@ head(Data)
 beren2 <- Data
 beren3 <- beren2[order(beren2$age) ,]
 write.csv(beren3, 'beren_new.csv', quote=F, row.names=FALSE)
+Feeds <- which(beren3$event == "bottle")
+avgMilk <- mean(beren3$value[Feeds])
+angFeed <- tapply(beren3$value[Feeds], beren3$age[Feeds], mean)
+varFeed <- tapply(beren3$value[Feeds], beren3$age[Feeds], var)
+totalFeed <- tapply(beren3$value[Feeds], beren3$age[Feeds], sum)
+numFeeds <- tapply(beren3$value[Feeds], beren3$age[Feeds], length)
+cor(beren3$value[Feeds], beren3$age [Feeds])
+cor.test(beren3$value[Feeds], beren3$age[Feeds])
+berenCor <- cor.test(beren3$value[Feeds], beren3$age[Feeds])
+summary(berenCor)
+berenANOVA <- aov(beren3$value[Feeds] ~ beren3$caregiver[Feeds])
+boxplot( beren3$value[Feeds] ~ beren3$caregiver[Feeds], xlab= "who gave the bottle" , ylab = "amount of milk consumed (oz)" )
+par(las=1, mar=c(5 ,5 ,1 ,1) , mgp=c(2, 0.5, 0) , tck=-0.01)
+plot(as.numeric(names(totalFeed)) , totalFeed , type="b" , pch=16, xlab=" age in days", ylab="ounces of milk")
+abline(h=mean(totalFeed) , lty=2, col='red')
+pdf("r02b-totalMilkByDay.pdf", height = 4, width = 4)
+par(las=1, mar=c(5 ,5 ,1 ,1) , mgp=c(2, 0.5, 0), tck=-0.01)
+plot(as.numeric(names(totalFeed)) , totalFeed, type="b", pch=16, xlab="age in days", ylab=" ounces of milk")
+abline(h=mean(totalFeed), lty=2, col='red')
+dev.off()
+Question 1: There's not enough data to be able to correlate with the variables.
+Question 2: The graph is very difficult to interpret because a lot of the data is compiled together due to the fact that there was high intervals on the x-axis.
+Self-Quiz: hypothesis= The amount of tylenol Beren takes increases with the amount of molars he gets.
+source("http://jonsmitchell.com/code/plotFxn02b.R")
+unique(beren3$event)
 
